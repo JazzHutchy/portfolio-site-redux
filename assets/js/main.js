fdb80d1c40c165ee63c5859a8b9a8ae870e4e1d2
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-01-16 09:41:03 +1100

    Add Python to skill section

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+function sentence() {
+  var x = "Hey this javascript "
+  var y = "works!"
+  document.write(x + y)
+  return
+}
+
+function add() {
+  var x = 5
+  var y = 9
+  var z = x + y + 5
+
+  document.write(z)
+  return
+}
+
+sentence()
+add()
