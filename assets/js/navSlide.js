13ba9a96a30ef79be7cf2fe0b195021a900c82ce
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-03-23 09:06:23 +1100

    jquery sliding fully operational

--- assets/js/navSlide.js
+++ assets/js/navSlide.js
@@ -1,11 +1,17 @@
 // jQuery: Slide page to section when button on the navbar is clicked
-$(document).ready(function() {
-  $("#slide-to").click(function() {
-    $("html, body").animate(
-      {
-        scrollTop: $("#test").offset().top
-      },
-      2000
-    )
-  })
+$(document).ready(function () {
+  setBindings()
 })
+
+function setBindings() {
+  $("nav ul li a").click(function (e) {
+    e.preventDefault()
+    var sectionID = e.currentTarget.id + "Section"
+    console.log('working')
+
+    $("html, body").animate({
+      scrollTop: $("#" + sectionID).offset().top
+    }, 1000)
+    console.log(sectionID)
+  })
+}
